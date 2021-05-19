01f04c5b302f10cf3ec3dda396f1156bc1cf1509
Joshyjojo <78159433+Joshyjojo@users.noreply.github.com>
2021-05-19 22:34:52 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 #include <cs50.h>
-
-int main(){
-    int pattern;
-    int rows;
-    int i,j,k;
-    pattern =get_int("Enter type of pattern (1 or 2) ");
-    rows =get_int("Number of rows");
-    if (pattern == 1){
-        for(i = 0;i<rows; i++){
-            for(j = rows;j>i; j--){
-                printf("#");
-            }
-            printf("\n");
-        }
+int main (void)
+{
+  int ch;
+  ch=get_int("Enter type of pattern (1 or 2) ");
+  int row;
+  int space;
+  if (ch==1)
+  {
+    row=get_int("Number of rows");
+    for (int i=row;i>=1;i--)
+    {
+      for (int j=1;j<=i;j++)
+        printf ("#");
+      printf ("\n");
     }
-    else{
-        for(i = 0;i<rows;i++){
-            for(j = rows-1;j>i;j--){
-                printf(" ");
-            }
-            for(k = 0;k <= j;k++)
-            {
-            printf("#");
-            }
-         printf("\n");
-        }
-    }
+  }
+  else if (ch==2)
+  {
+    row=get_int("Number of rows");
+    space=1;
+    for (int i=row;i>=1;i--)
+    {
+      for (int j=1;j<=i-1;j++)
+        printf (" ");
+      for (int k=1;k<=space;k++)
+        printf ("#");
+      printf ("\n");
+      space++;  
+     }
+   }             
+    else         
+    printf("Inavalid choice");
 }
